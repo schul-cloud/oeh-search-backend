dd143f73ee15910f323ddb39a76b16cfab7cb006
Ioannis Koumarelas <ioannis.koumarelas@hpi.de>
2020-10-23 16:03:20 +0200

    SC-7032 - Antares PoC
    A first implementation for the Antares spider. It provides a number of different functionalities, including:
    1. Connects through a white-listed and protected through authentication proxy server.
    2. Paginates through the harvesting API across states and counties (Kreis).
    3. Properly parses the elements from their special XML format.
    4. Translates value encodings, from their EAF/LOM representation to their alphanumeric and human-understandable one.

--- converter/spiders/__init__.py
+++ converter/spiders/__init__.py
@@ -2,3 +2,8 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+from pathlib import Path
+
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent.parent
