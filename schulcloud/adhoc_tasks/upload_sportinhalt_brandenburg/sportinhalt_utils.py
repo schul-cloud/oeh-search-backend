8eb7cc0ef603925a4938db624e7e376e25d45acc
Ioannis Koumarelas <ioannis.koumarelas@gmail.com>
2021-05-12 17:32:21 +0200

    Revert "SC-9056 - Modify Sportinhalt content at Edu-Sharing"
    This reverts commit 08a227db

--- schulcloud/adhoc_tasks/upload_sportinhalt_brandenburg/sportinhalt_utils.py
+++
@@ -1,7 +0,0 @@
-def get_base_headers(auth_response):
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Cookie": auth_response.headers.get("Set-Cookie"),
-    }
-    return headers
